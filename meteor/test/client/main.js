0d9933411342d132fa57c6df7365e0fb580054f6
Shariq Masood <shariq.masood@hotmail.com>
2020-02-27 16:03:06 +0500

    no message

--- meteor/test/client/main.js
+++ meteor/test/client/main.js
@@ -1,5 +1,3 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -17,24 +15,4 @@ const App = () =>{
 Meteor.startup(()=> {
   ReactDOM.render(<App/>, document.querySelector('.container'))
 
-})
-
-
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
 });
