f11ddcaa77d904495b05da0f135a6a62655df37b
Shariq Masood <shariq.masood@hotmail.com>
2020-02-27 15:56:47 +0500

    meteor project setup

---
+++ meteor/test/client/main.js
@@ -0,0 +1,40 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.html';
+
+
+const App = () =>{
+  return (
+    <div>
+      React App #2
+    </div>
+  );
+};
+
+Meteor.startup(()=> {
+  ReactDOM.render(<App/>, document.querySelector('.container'))
+
+})
+
+
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
